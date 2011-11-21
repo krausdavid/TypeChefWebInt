dfcf46fcf1e104c07b73eb4d760696b56ae65522
David Kraus <krausdavid@informatik.uni-marburg.de>
2011-11-21 15:27:19 +0100

    first work on useDBGFileCheker.sh script

--- scripts/useDBGFileCheck.sh
+++ scripts/useDBGFileCheck.sh
@@ -1,1 +1,10 @@
-Script for use DBGFileChecker with script
+#!/bin/bash -e
+#!/bin/bash -vxe
+
+filesToProcess(){
+  folder="fileLists"  
+  location="fileLists/$1"
+  echo $location
+}
+
+filesToProcess
