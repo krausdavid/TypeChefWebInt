1d8ec0c7e872c71365ad60fa077cd713b649689e
Alexander Eifler <eifx-eifler@freenet.de>
2012-02-17 00:17:33 +0100

    tidy up the jcpp folder

--- jcpp/KBuildMiner/miner.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-mvn scala:run -q -DmainClass=gsd.buildanalysis.linux.KBuildMinerMain -DaddArgs="%1|%2|%3|%4|%5|%6|%7"
-rem mvn -e exec:java -Dexec.mainClass=gsd.buildanalysis.linux.KBuildMinerMain -Dexec.args="%1|%2|%3|%4|%5|%6|%7"
--- jcpp/KBuildMiner/miner.sh
+++
@@ -1,1 +0,0 @@
-mvn scala:run -q -DmainClass=gsd.buildanalysis.linux.KBuildMinerMain -DaddArgs="%1|%2|%3|%4|%5|%6|%7"
