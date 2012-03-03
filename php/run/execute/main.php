dd8c42cb53c99d223ddd20e36d3ffc245fe3424a
Alexander Eifler <eifx-eifler@freenet.de>
2012-03-03 18:29:40 +0100

    little changes

--- php/run/execute/main.php
+++ php/run/execute/main.php
@@ -17,14 +17,17 @@
  * =====================================================
  */
 	//TODO Projekt dynamisch machen 
-	echo "Anfrage wird bearbeitet...<br>";
+	echo "Anfrage wird bearbeitet...<br><br>";
+	echo "Web_WriteFileList wird ausgeführt<br>";
 	echo shell_exec("java -jar ../java/web_WriteFileList/Web_WriteFileList.jar busybox ".$_GET['chkStr']." /app/home/eifx/public_html/TypeChefWebInt/projects/");
 	
 	//echo shell_exec("../scripts/useJCPPFileChecker.sh busybox.lst /app/archive/kos/share/TypeChef/busybox/busybox-1.18.5 busybox.flags");
+	echo "<br><br>analyzeBusybox.sh wird ausgeführt<br>";
 	echo shell_exec("/app/archive/kos/share/TypeChef/busybox/analyzeBusybox.sh");
+	echo "<br><br>Web_ProjectInitializator.jar wird ausgeführt<br>";
 	echo shell_exec("java -jar ../java/web_ProjectInitializator/Web_ProjectInitializator.jar /app/archive/kos/share/TypeChef/busybox/busybox-1.18.5 busybox /app/home/eifx/public_html/TypeChefWebInt/global_settings.xml");
 	//echo shell_exec("java -jar ../java/web_ProjectInitializator/Web_ProjectInitializator.jar /app/archive/kos/share/TypeChef/busybox/busybox-1.18.5 busybox /app/home/eifx/public_html/TypeChefWebInt/global_settings.xml");
-	echo "DONE!!!";
+	echo "<br><br>DONE!!!";
 	
 	$template->assign("title", "Skriptauswertung");
 	$template->assign("template", "./execute/main.tpl"); 
--- php/templates/tpl/project/main.tpl
+++ php/templates/tpl/project/main.tpl
@@ -1,7 +1,7 @@
 ﻿{strip}
 	Files:<br>
 	{foreach from=$files item=act_file}
-		<a href="{$_wud}/project?files={$files_get}&project={$project}&choosen_file={$act_file}">{$act_file}</a>
+		<a href="{$_wud}/project?files={$files_get}&project={$project}&choosen_file={$act_file}">{$act_file}</a>&nbsp;
 	{/foreach}
 	<hr>
 	{if $smarty.get.choosen_file neq ""}
