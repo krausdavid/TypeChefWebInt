6c88fb77cafd9b3e424790c9113f7393291f1389
Alexander Eifler <eifx-eifler@freenet.de>
2012-03-04 22:12:39 +0100

    - Web-Design: Put the website in the middle --> Bug: In IE not possible - Put TODO in the ISSUE-Part of GitHub

--- php/templates/php/%%98^98D^98DD9168%%main.tpl.php
+++ php/templates/php/%%98^98D^98DD9168%%main.tpl.php
@@ -1,5 +1,20 @@
-<?php /* Smarty version 2.6.26, created on 2012-03-03 18:07:09
+<?php /* Smarty version 2.6.26, created on 2012-03-04 21:05:59
          compiled from ./project/main.tpl */ ?>
-﻿<?php echo 'Files:<br>'; ?><?php $_from = $this->_tpl_vars['files']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }if (count($_from)):
+﻿Files:<br>
+<?php $_from = $this->_tpl_vars['files']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }if (count($_from)):
     foreach ($_from as $this->_tpl_vars['act_file']):
-?><?php echo '<a href="'; ?><?php echo $this->_tpl_vars['_wud']; ?><?php echo '/project?files='; ?><?php echo $this->_tpl_vars['files_get']; ?><?php echo '&project='; ?><?php echo $this->_tpl_vars['project']; ?><?php echo '&choosen_file='; ?><?php echo $this->_tpl_vars['act_file']; ?><?php echo '">'; ?><?php echo $this->_tpl_vars['act_file']; ?><?php echo '</a>&nbsp;'; ?><?php endforeach; endif; unset($_from); ?><?php echo '<hr>'; ?><?php if ($_GET['choosen_file'] != ""): ?><?php echo 'File: '; ?><?php echo $_GET['choosen_file']; ?><?php echo '<br><pre>'; ?><?php echo $this->_tpl_vars['filecontent']; ?><?php echo '</pre>'; ?><?php endif; ?><?php echo ''; ?>
+?>
+	<a href="<?php echo $this->_tpl_vars['_wud']; ?>
+/project?files=<?php echo $this->_tpl_vars['files_get']; ?>
+&project=<?php echo $this->_tpl_vars['project']; ?>
+&choosen_file=<?php echo $this->_tpl_vars['act_file']; ?>
+"><?php echo $this->_tpl_vars['act_file']; ?>
+</a>
+<?php endforeach; endif; unset($_from); ?>
+<hr>
+<?php if ($_GET['choosen_file'] != ""): ?>
+	File: <?php echo $_GET['choosen_file']; ?>
+<br>
+	<pre><?php echo $this->_tpl_vars['filecontent']; ?>
+</pre>
+<?php endif; ?>
--- php/templates/tpl/project/main.tpl
+++ php/templates/tpl/project/main.tpl
@@ -1,11 +1,9 @@
-﻿{strip}
-	Files:<br>
-	{foreach from=$files item=act_file}
-		<a href="{$_wud}/project?files={$files_get}&project={$project}&choosen_file={$act_file}">{$act_file}</a>&nbsp;
-	{/foreach}
-	<hr>
-	{if $smarty.get.choosen_file neq ""}
-		File: {$smarty.get.choosen_file}<br>
-		<pre>{$filecontent}</pre>
-	{/if}
-{/strip}
+﻿Files:<br>
+{foreach from=$files item=act_file}
+	<a href="{$_wud}/project?files={$files_get}&project={$project}&choosen_file={$act_file}">{$act_file}</a>
+{/foreach}
+<hr>
+{if $smarty.get.choosen_file neq ""}
+	File: {$smarty.get.choosen_file}<br>
+	<pre>{$filecontent}</pre>
+{/if}
