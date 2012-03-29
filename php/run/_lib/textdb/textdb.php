cfa104ea23ecc8b3b7872294b9154f88aaa21e89
Alexander Eifler <eifx-eifler@freenet.de>
2012-03-29 17:24:37 +0200

    added some comments and an abort criteria.

--- php/run/_lib/textdb/textdb.php
+++ php/run/_lib/textdb/textdb.php
@@ -19,15 +19,18 @@
 class textdb{
 	var $dbc;
 
+	//Connect the db with the local text-variable
 	function connect($filepath){
 		$var="";
 		$handle = fopen($filepath,"w+");
 		while(!feof($handle)){
 			$var.=fgets($handle)."\r\n";
 		}
+		fclose($handle);
 		$this->dbc = $var;
 	}
 
+	//Returns an array with selected fields
 	function select_all($fieldname,$fieldval){
 		$arr_all = explode("\r\n",$this->dbc);
 		if(count($arr_all)>0){
@@ -38,6 +41,10 @@ class textdb{
 					if($fieldname==$arr_header[$i]){
 						$fieldID=$i;
 					}
+				}
+				
+				if($fieldID==-1){
+					return false;
 				}
 				
 				for($i=1;$i<count($arr_all);$i++){
