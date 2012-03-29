d8fc0ff490e2b844966a1073cb32ba6901420144
Alexander Eifler <eifx-eifler@freenet.de>
2012-03-29 16:46:38 +0200

    added files for login / logout

---
+++ php/run/account/login.php
@@ -0,0 +1,22 @@
+﻿<?php
+
+/*
+ * #####################################################
+ * #                                                   #
+ * # TYPECHEF WEB INTERFACE                            #
+ * #                                                   #
+ * # Copyright (c) by EifX & KrausDavid                #
+ * #                                                   #
+ * #                    eifx@informatik.uni-marburg.de #
+ * #              krausdavid@informatik.uni-marburg.de #
+ * #                                                   #
+ * #####################################################
+ *
+ * ===Notes=============================================
+ * There are currently no notes.
+ * =====================================================
+ */
+
+$template->assign("title", "Login");
+$template->assign("template", "./account/login.tpl"); 
+?>
---
+++ php/run/account/logout.php
@@ -0,0 +1,20 @@
+﻿<?php
+
+/*
+ * #####################################################
+ * #                                                   #
+ * # TYPECHEF WEB INTERFACE                            #
+ * #                                                   #
+ * # Copyright (c) by EifX & KrausDavid                #
+ * #                                                   #
+ * #                    eifx@informatik.uni-marburg.de #
+ * #              krausdavid@informatik.uni-marburg.de #
+ * #                                                   #
+ * #####################################################
+ *
+ * ===Notes=============================================
+ * There are currently no notes.
+ * =====================================================
+ */
+
+?>
