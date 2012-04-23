ac093e670f1a71c10fc7d0293d581a32016cbc7a
Alexander Eifler <eifx-eifler@freenet.de>
2012-04-23 22:53:28 +0200

    - fixed some errors - added some features

--- php/run/project/main.php
+++ php/run/project/main.php
@@ -31,6 +31,7 @@ $template->assign("project", $_GET['project']);
 $template->assign("files_get", $_GET['files']);
 
 if($_GET['choosen_file']!=""){
+	//Get the file extension
 	$fileEXTarr = explode(".",$_GET['choosen_file']);
 	$fileEXT = "";
 	
@@ -38,34 +39,64 @@ if($_GET['choosen_file']!=""){
 		$fileEXT .= ".".$fileEXTarr[$i];
 	}
 	
-	//Replace < and > Tags. HTML display this characters wrong
+	//Replace < and > Tags. HTML display this characters wrong. This is only for
+	//not syntaxable files
 	$output_file = tools::read_file($PROJECT_PATH.$_GET['files'].$fileEXT,false);
 	
-	//$output_file = str_replace("<","&lt;",$output_file);
-	//$output_file = str_replace(">","&gt;",$output_file);
-
-	//Syntax Higlightning
+	$isSyntaxable = false;
+	if($fileEXT==".c" || $fileEXT==".c.interface" || $fileEXT==".interface" || $fileEXT==".pi"){
+		$isSyntaxable = true;
+	}
+	
+	if(!$isSyntaxable){
+		$output_file = str_replace("<","&lt;",$output_file);
+		$output_file = str_replace(">","&gt;",$output_file);
+	}
+	
+	//Syntax higlightning
 	if($fileEXT==".c"){
+		$geshi = new GeSHi($output_file, 'c');
+		$output_file = $geshi->parse_code();
+	}
+	if($fileEXT==".c.interface"){
+		$geshi = new GeSHi($output_file, 'xml');
+		$output_file = $geshi->parse_code();
+	}
+	if($fileEXT==".interface"){
+		$geshi = new GeSHi($output_file, 'xml');
+		$output_file = $geshi->parse_code();
+	}
+	if($fileEXT==".pi"){
 		$geshi = new GeSHi($output_file, 'c');
 		$output_file = $geshi->parse_code();
 	}
 	
 	$output_file = str_replace("\t"," ",$output_file);
+	$output_file = str_replace("\r","",$output_file);
 	$output_file_arr = explode("\n",$output_file);
 	
-	
 	for($i=0;$i<count($output_file_arr);$i++){
--		$spaces_display ="";
+		//Spaces-Display saves the spaces between numbers
+		//Example:
+		//9  |
+		//10 | 
+		//It try to put the correct distances between numbers and the separator
+		$spaces_display ="";
 		for($j=0;$j<(strlen(count($output_file_arr))-strlen($i+1));$j++){
 			$spaces_display.=" ";
 		}
--		$spaces_wrap ="";
-		for($j=0;$j<(strlen(count($output_file_arr)));$j++){
-			$spaces_wrap.=" ";
+		
+		//Wordwrap if the given text is not syntaxable
+		if(!$isSyntaxable){
+			$spaces_wrap ="";
+			for($j=0;$j<(strlen(count($output_file_arr)));$j++){
+				$spaces_wrap.=" ";
+			}
+			$out[$i]['line'] = wordwrap($output_file_arr[$i],100,"<br/>".$spaces_wrap." | ",true);
+		}else{
+			$out[$i]['line'] = $output_file_arr[$i];
 		}
 		
-		//$out[$i]['line'] = wordwrap($output_file_arr[$i],100,"<br/>".$spaces_wrap." | ",true);
-		$out[$i]['line'] = $output_file_arr[$i];
 		$out[$i]['nr'] = ($i+1).$spaces_display." | ";
 	}
 	
