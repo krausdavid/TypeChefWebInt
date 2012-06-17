46a9a6640140120488a4b113c18356809f210d8b
Alexander Eifler <eifx-eifler@freenet.de>
2012-06-17 23:39:52 +0200

    Project Reload, added recompile.sh for faster recompiling of given projects

---
+++ java/recompile.sh
@@ -0,0 +1,12 @@
+java -jar Web_DeleteProject.jar busybox1185 /app/home/eifx/public_html/TypeChefWebInt/global_settings.xml
+java -jar Web_DeleteProject.jar busybox1185_n /app/home/eifx/public_html/TypeChefWebInt/global_settings.xml
+java -jar Web_DeleteProject.jar linux26333 /app/home/eifx/public_html/TypeChefWebInt/global_settings.xml
+java -jar Web_DeleteProject.jar busybox1185_busybox1185_n_compare /app/home/eifx/public_html/TypeChefWebInt/global_settings.xml
+java -jar Web_ProjectInitializator.jar /app/archive/kos/share/TypeChef/busybox/busybox-1.18.5 busybox1185 BusyBox 1.18.5 EifX /app/home/eifx/public_html/TypeChefWebInt/global_settings.xml
+java -jar Web_ProjectInitializator.jar /app/archive/kos/share/TypeChef/busybox/busybox-1.18.5 busybox1185_n BusyBox 1.18.5 EifX /app/home/eifx/public_html/TypeChefWebInt/global_settings.xml
+java -jar Web_ProjectInitializator.jar /app/archive/kos/share/TypeChef/linux/linux-2.6.33.3 linux26333 Linux 2.6.33.3 EifX /app/home/eifx/public_html/TypeChefWebInt/global_settings.xml
+java -jar Web_ProjectCompare.jar busybox1185 busybox1185_n EifX /app/home/eifx/public_html/TypeChefWebInt/global_settings.xml
+java -jar Web_TreeViewInitializator.jar busybox1185 /app/home/eifx/public_html/TypeChefWebInt/global_settings.xml
+java -jar Web_TreeViewInitializator.jar busybox1185_n /app/home/eifx/public_html/TypeChefWebInt/global_settings.xml
+java -jar Web_TreeViewInitializator.jar linux26333 /app/home/eifx/public_html/TypeChefWebInt/global_settings.xml
+java -jar Web_TreeViewInitializator.jar busybox1185_busybox1185_n_compare /app/home/eifx/public_html/TypeChefWebInt/global_settings.xml
