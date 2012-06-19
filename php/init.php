7a8a7347956d516e4182865f4da455ef491231bb
Alexander Eifler <eifx-eifler@freenet.de>
2012-06-19 09:33:37 +0200

    Website VERSION_UPGRADE 0.2.2.0 - Permalinks now work

--- php/init.php
+++ php/init.php
@@ -50,7 +50,7 @@ foreach($xml->global->projects[0]->attributes() as $a => $b) {
 }
 
 define("WEBSITE_NAME", "TypeChefWebInt");
-define("WEBSITE_VERSION", "0.2.1.4");
+define("WEBSITE_VERSION", "0.2.2.0");
 define("WEBSITE_AUTOR", "Alexander 'EifX' Eifler, David 'hullleman' Kraus");
 define("WEBSITE_SESSION_NAME", "session");
 
