691672bd8025391be2b69b9264817e9dcc0570bb
Alexander Eifler <eifx-eifler@freenet.de>
2012-06-25 18:23:20 +0200

    Web_ProjectInitializator VERSION_UPGRADE 0.1.3.2 - Little Bugfixes with parameters

--- java/javaProjectFiles/web_ProjectInitializator/src/Web_ProjectInitializator.java
+++ java/javaProjectFiles/web_ProjectInitializator/src/Web_ProjectInitializator.java
@@ -13,7 +13,7 @@ import tcwi.tools.Tools;
 import tcwi.xml.Parser;
 
 public class Web_ProjectInitializator {
-	private static final String VERSION = "0.1.3.1";
+	private static final String VERSION = "0.1.3.2";
 	private static final String AUTHORS = "EifX & hulllemann";
 	private static ArrayList<ErrorFile> files = new ArrayList<ErrorFile>();
 	private static Check check = new Check();
@@ -118,11 +118,12 @@ public class Web_ProjectInitializator {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		if(args.length!=7){
+		if(args.length!=8){
 			System.out.println("Help - Web_ProjectInitializator "+VERSION+" by "+AUTHORS);
 			System.out.println("----------------------------------------------------");
 			System.out.println("\nUsage: Web_ProjectInitializator [PROJECTPATH] [PROJECTNAME] [PROJECTFULLNAME] ");
-			System.out.println("                                [PROJECTVERSION] [PROJECTAUTHOR] [GLOBAL_SETTINGS]");
+			System.out.println("                                [PROJECTVERSION] [PROJECTAUTHOR] [DELTAPROJECT]");
+			System.out.println("                                [DELTAMAIN] [GLOBAL_SETTINGS]");
 			System.out.println("\n[PROJECTPATH]");
 			System.out.println("     Absolute Path for scan for TypeChef-Files\n");
 			System.out.println("[PROJECTNAME]");
@@ -136,7 +137,7 @@ public class Web_ProjectInitializator {
 			System.out.println("[DELTAPROJECT]");
 			System.out.println("     true or false, if the project has deltas\n");
 			System.out.println("[DELTAMAIN]");
-			System.out.println("     The main-project who belongs to this delta. Type _ if this is the main-project\n");
+			System.out.println("     The main-project who belongs to this delta. Type _ if this is the\nmain-project\n");
 			System.out.println("[GLOBAL_SETTINGS]");
 			System.out.println("     Absolute Path for the global_settings.xml\n     (include the name of the settings file)\n");
 		}else{
