000a712f64f55a54006c768a4a9a189453b554f4
Alexander Eifler <eifx-eifler@freenet.de>
2012-08-29 23:09:04 +0200

    Web_TreeViewInitializator VERSION_UPGRADE 0.5.1.2 - Fixed the folder-display in treeview

--- java/javaProjectFiles/web_TreeViewInitializator/src/Web_TreeViewInitializator.java
+++ java/javaProjectFiles/web_TreeViewInitializator/src/Web_TreeViewInitializator.java
@@ -12,7 +12,7 @@ import tcwi.TCWIFile.ErrorFile;
 
 public class Web_TreeViewInitializator {
 
-	private static final String VERSION = "0.5.1.1";
+	private static final String VERSION = "0.5.1.2";
 	private static final String AUTHORS = "EifX & hulllemann";
 	private static ArrayList<String> javascript = new ArrayList<String>();
 	private static String folderSeparator = Check.folderSeparator();
@@ -104,7 +104,7 @@ public class Web_TreeViewInitializator {
 					str = eFile.getPath();
 				}
 				
-				FolderElem e = new FolderElem(eFile.haveErrors()||eFile.getCompileError()==ErrorState.NOWCOMPILEERROR||eFile.getCompileError()==ErrorState.COMPILEERROR,str);
+				FolderElem e = new FolderElem(eFile.isParserErrorCountChanged()||eFile.isTypeErrorCountChanged(),str);
 				addInFailFolders(e);
 			}
 		}
