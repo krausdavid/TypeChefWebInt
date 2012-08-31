3b3900981500b6615e2951fb8f4fe365412db676
Alexander Eifler <eifx-eifler@freenet.de>
2012-08-31 13:58:29 +0200

    Web_ProjectDelete VERSION_UPGRADE 0.0.2.0 - Renamed Project - Actualized recompile.sh

---
+++ java/javaProjectFiles/web_DeleteProject/src/Web_ProjectDelete.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import tcwi.fileHandler.Check;
+import tcwi.xml.Parser;
+
+
+public class Web_ProjectDelete {
+	private static final String VERSION = "0.0.2.0";
+	private static final String AUTHORS = "EifX & hulllemann";
+	private static Parser parser;
+	
+	public static void main(String[] args) {
+		if(args.length!=2){
+			System.out.println("Help - web_DeleteProject "+VERSION+" by "+AUTHORS);
+			System.out.println("----------------------------------------------------");
+			System.out.println("\nUsage: web_DeleteProject [PROJECTNAME] [GLOBAL_SETTINGS]");
+			System.out.println("\n[PROJECTNAME]");
+			System.out.println("\n     Project name\n");
+			System.out.println("\n[GLOBAL_SETTINGS]");
+			System.out.println("\n     Absolute Path for the global_settings.xml\n     (include the name of the settings file)\n");
+		}else{
+			
+			//Init the Parser
+			parser = new Parser(args[1]);
+			String webIntProjectsPath = parser.getSetting_ProjectPath();
+			String webIntTreeviewPath = parser.getSetting_TreeviewPath();
+			
+			File f = new File(webIntTreeviewPath + Check.folderSeparator() + args[0] + ".js");
+			f.delete();
+			f = new File(webIntTreeviewPath + Check.folderSeparator() + args[0] + ".nochk.js");
+			f.delete();
+			f = new File(webIntTreeviewPath + Check.folderSeparator() + args[0] + ".err.js");
+			f.delete();
+			f = new File(webIntTreeviewPath + Check.folderSeparator() + args[0] + ".err.nochk.js");
+			f.delete();
+			f = new File(webIntProjectsPath + Check.folderSeparator() + args[0] + ".project.xml");
+			f.delete();
+			f = new File(webIntProjectsPath + Check.folderSeparator() + args[0] + ".lst");
+			f.delete();
+			System.out.println("Project "+ args[0] +" successfully deleted!");
+		}
+	}
+
+}
